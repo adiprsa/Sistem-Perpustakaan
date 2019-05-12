3b7ef96a629609abb7b28af7dd7dc790df6b64d6
erilavindi <49615441+erilavindi@users.noreply.github.com>
2019-05-12 17:08:07 +0700

    laporan

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,52 +1,76 @@
-            <!-- ============================================================== -->
-            <!-- footer -->
-            <!-- ============================================================== -->
-            <div class="footer">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                            Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
-                        </div>
-                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                            <div class="text-md-right footer-links d-none d-sm-block">
-                                <a href="javascript: void(0);">About</a>
-                                <a href="javascript: void(0);">Support</a>
-                                <a href="javascript: void(0);">Contact Us</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- ============================================================== -->
-            <!-- end footer -->
-            <!-- ============================================================== -->
-        </div>
-        <!-- ============================================================== -->
-        <!-- end wrapper  -->
-        <!-- ============================================================== -->
+
     </div>
     <!-- ============================================================== -->
-    <!-- end main wrapper  -->
+    <!-- footer -->
     <!-- ============================================================== -->
-    
-    <!-- Optional JavaScript -->
-    <!-- jquery 3.3.1 -->
-    <!-- slimscroll js -->
-    <script src="<?=base_url()?>/assets/vendor/slimscroll/jquery.slimscroll.js"></script>
-    <!-- main js -->
-    <script src="<?=base_url()?>/assets/libs/js/main-js.js"></script>
-    <!-- chart chartist js -->
-    <script src="<?=base_url()?>/assets/vendor/charts/chartist-bundle/chartist.min.js"></script>
-    <!-- sparkline js -->
-    <script src="<?=base_url()?>/assets/vendor/charts/sparkline/jquery.sparkline.js"></script>
-    <!-- morris js -->
-    <script src="<?=base_url()?>/assets/vendor/charts/morris-bundle/raphael.min.js"></script>
-    <script src="<?=base_url()?>/assets/vendor/charts/morris-bundle/morris.js"></script>
-    <!-- chart c3 js -->
-    <script src="<?=base_url()?>/assets/vendor/charts/c3charts/c3.min.js"></script>
-    <script src="<?=base_url()?>/assets/vendor/charts/c3charts/d3-5.4.0.min.js"></script>
-    <script src="<?=base_url()?>/assets/vendor/charts/c3charts/C3chartjs.js"></script>
-    <script src="<?=base_url()?>/assets/libs/js/dashboard-ecommerce.js"></script>
+    <div class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                   Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
+               </div>
+               <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                <div class="text-md-right footer-links d-none d-sm-block">
+                    <a href="javascript: void(0);">About</a>
+                    <a href="javascript: void(0);">Support</a>
+                    <a href="javascript: void(0);">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- end footer -->
+<!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- end wrapper  -->
+<!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- end main wrapper  -->
+<!-- ============================================================== -->
+<!-- Optional JavaScript -->
+<!-- jquery 3.3.1 -->
+<script src="<?php echo base_url('assets/vendor/jquery/jquery-3.3.1.min.js') ?>"></script>
+<!-- bootstap bundle js -->
+<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.js') ?>"></script>
+<!-- slimscroll js -->
+<script src="<?php echo base_url('assets/vendor/slimscroll/jquery.slimscroll.js') ?>"></script>
+<!-- main js -->
+<script src="<?php echo base_url('assets/libs/js/main-js.js') ?>"></script>
+<!-- chart chartist js -->
+<script src="<?php echo base_url('assets/vendor/charts/chartist-bundle/chartist.min.js') ?>"></script>
+<!-- sparkline js -->
+<script src="<?php echo base_url('assets/vendor/charts/sparkline/jquery.sparkline.js') ?>"></script>
+<!-- morris js -->
+<script src="<?php echo base_url('assets/vendor/charts/morris-bundle/raphael.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/vendor/charts/morris-bundle/morris.js') ?>"></script>
+<!-- chart c3 js -->
+<script src="<?php echo base_url('assets/vendor/charts/c3charts/c3.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/vendor/charts/c3charts/d3-5.4.0.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/vendor/charts/c3charts/C3chartjs.js') ?>"></script>
+<!-- <script src="assets/libs/js/dashboard-ecommerce.js"></script> -->
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/jquery.dataTables.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/extensions/export/buttons.flash.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/extensions/export/jszip.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/extensions/export/pdfmake.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/extensions/export/vfs_fonts.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/extensions/export/buttons.html5.min.js') ?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/plugins/jquery-datatable/extensions/export/buttons.print.min.js') ?>"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('table').DataTable({
+            searching : true,
+            ordering : true,
+            paging : false,
+            dom : 'Bfrtip',
+            responsive : true,
+            buttons : ['excel']
+        });
+    });
+</script>
 </body>
- 
+
 </html>
