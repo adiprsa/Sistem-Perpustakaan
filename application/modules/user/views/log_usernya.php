3f6db0d6bd1d5178d86c0c830cd06156d01e1b23
romrozky <romadhoni@ptdes.net>
2019-05-24 05:29:03 +0700

    removed button

--- application/modules/user/views/log_usernya.php
+++ application/modules/user/views/log_usernya.php
@@ -35,7 +35,6 @@
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="card">
                             <div class="card-header">
-                                <button type='button' class='btn btn-success tambah'>Tambah</button>
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
